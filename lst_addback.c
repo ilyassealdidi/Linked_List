e85e423a8093c73aee9462fa8063c60e8e68f844
Ilyasse <95250342+ilyassealdidi@users.noreply.github.com>
2024-04-03 01:05:56 +0000

    first commit

---
+++ lst_addback.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lst_addback.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ialdidi <ialdidi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/03 00:38:51 by ialdidi           #+#    #+#             */
+/*   Updated: 2024/04/03 00:38:59 by ialdidi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	lst_addback(t_list **lst, t_list *new)
+{
+	t_list	*last;
+
+	if (!lst || !new)
+		return ;
+	if (!*lst)
+	{
+		*lst = new;
+		(*lst)->next = new;
+		(*lst)->previous = new;
+	}
+	else
+	{
+		last = lst_last(*lst);
+		last->next = new;
+		(*lst)->previous = new;
+		new->next = *lst;
+		new->previous = last;
+	}
+}
---
+++ lst_new.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lst_new.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ialdidi <ialdidi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/03 00:35:40 by ialdidi           #+#    #+#             */
+/*   Updated: 2024/04/03 00:40:43 by ialdidi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*lst_new(void *content)
+{
+	t_list	*head;
+
+	head = (t_list *)malloc(sizeof(t_list));
+	if (!head)
+		return (NULL);
+	head->content = content;
+	head->previous = NULL;
+	head->next = NULL;
+	return (head);
+}
